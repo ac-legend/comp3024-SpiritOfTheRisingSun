9a562b514c7feb47df3ba8510623d5f35781cd77
ac-legend <129148182+ac-legend@users.noreply.github.com>
2023-10-12 13:16:48 +1100

    Fix Waterfall

--- Assets/Scripts/Waterfall/WaterfallAnimation.cs
+++ Assets/Scripts/Waterfall/WaterfallAnimation.cs
@@ -18,11 +18,13 @@ namespace GenshinImpactMovementSystem
         private void Update()
         {
             // Calculate new offset values based on time and scrollSpeed
-            float offsetX = Time.time * scrollSpeed;
-            float offsetY = Time.time * scrollSpeed;
+            //float offsetX = Time.time * scrollSpeed;
+            float offsetX = 2;
+            float offsetY = -(Time.time * scrollSpeed);
 
             // Set the new offset values for the "Stylize water" texture
-            material.SetTextureOffset("Stylize water", new Vector2(offsetX, offsetY));
+            material.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
+            material.mainTextureOffset = new Vector2(offsetX, offsetY);
         }
     }
 }
